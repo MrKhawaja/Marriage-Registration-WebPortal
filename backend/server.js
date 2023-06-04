8f37a493ee6834056fb2e815e242a2261281dbe9
Mr.Khawaja <alveehawak360@gmail.com>
2023-06-04 23:51:22 +0600

    made the login function

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const port = 3000;
+const jwt_secret = "secret";
+
+
+
+app.use(express.urlencoded({ extended: true }));
 
 
 const db = mysql.createConnection({
@@ -28,10 +35,55 @@ db.connect((err) => {
 );
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-    }
-);
+app.post('/auth/register', (req, res) => {
+    const name = req.body.name.toLowerCase();
+    const email = req.body.email;
+    var password = req.body.password;
+
+
+    db.query('select id from users where email = ? union select id from users where name = ?', [email,name], (err, result) => {
+        if (err) throw err
+        if (result.length > 0) {
+            return res.status(400).send("User already exists")
+        }
+
+        bcrypt.hash(password, 10, (err, hash) => {
+            if (err) throw err
+            password = hash
+            console.log(name,email,password)
+            db.query('insert into users (name, email, password) values (?,?,?)', [name, email, password], (err, result) => {
+                if (err) throw err
+                res.status(200).send("User Registered")
+            })
+        })
+    })
+
+
+
+})
+
+app.post('/auth/login', (req, res) => {
+    const name = req.body.name.toLowerCase();
+    const password = req.body.password;
+    db.query('select * from users where name = ?', [name], (err, result) => {
+        if (err) throw err
+        if (result.length === 0) {
+            return res.status(400).send("User not found")
+        }
+        bcrypt.compare(password, result[0].password, (err, response) => {
+            if (err) throw err
+            if (response) {
+                const token = jwt.sign({ id: result[0].id,name:name,email:result[0].email }, jwt_secret)
+                return res.status(200).send(token)
+            }
+            else {
+                return res.status(400).send("Wrong Password")
+            }
+        })
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
